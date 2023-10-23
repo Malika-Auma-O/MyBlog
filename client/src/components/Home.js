391af2ca11cf7068d4a4e0bb8fcf38a24225e5b2
Malika-Auma-O <mamajedeia@gmail.com>
2023-10-23 21:02:52 +0300

    added favicon and logo

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,31 @@
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Hero from "../images/blogHero1.avif"
+
 function Home() {
-    return ( 
-        <div>
-            Home
-        </div>
-     );
+
+    const customBolderText = {
+        fontWeight: '800', 
+      };
+
+    const customBoldText = {
+        fontWeight: '600', 
+      };
+  return (
+    <div>
+        <Card className="bg-dark text-white border-0 rounded-0">
+      <Card.Img src={Hero} alt="Card image" />
+      <Card.ImgOverlay className='pt-5 mt-5 '>
+        <Card.Title style={customBoldText} className="display-6 pt-5 mt-5">My Coding Journey</Card.Title>
+        <Card.Title  className='text-white display-2 pt-5 mt-5' style={customBolderText} >
+          Helpful Notes and Thoughts as I Learn Coding.
+        </Card.Title>
+        <Button className='mt-4' variant="success">All Blogs</Button>
+      </Card.ImgOverlay>
+    </Card>
+    </div>
+    
+  );
 }
 
 export default Home;
--- client/src/components/MainNavbar.js
+++ client/src/components/MainNavbar.js
@@ -5,6 +5,8 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import FormControl from 'react-bootstrap/FormControl';
+import Image from 'react-bootstrap/Image';
+import logo from "../images/blogLogo1.png"
 
 
 
@@ -15,23 +17,29 @@ function MainNavbar() {
   return (
     <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">DevNotes Blog</Navbar.Brand>
+      <Navbar.Brand href="/home">
+        <Image src={logo} alt="Logo" height="40" />
+      </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#features">Home</Nav.Link>
-            <Nav.Link href="#pricing">About</Nav.Link>
-            <Nav.Link href="#pricing">contact</Nav.Link>
+            <Nav.Link href="/home">Home</Nav.Link>
+            <Nav.Link href="/blogs">Blogs</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
+            <Nav.Link href="/contact">Contact</Nav.Link>
           </Nav>
           
         </Navbar.Collapse>
         <Nav>
-        <Form className="d-flex">
+        <Form
+         className="d-flex">
           <InputGroup>
             <FormControl
               type="search"
               placeholder="Search"
               aria-label="Search"
+              size="sm"
+              
             />
             <Button variant="outline-success" id="search-button">
               <i className="bi bi-search"></i> {/* Magnifying glass icon */}
