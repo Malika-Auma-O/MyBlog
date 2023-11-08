9c6e0a59d40d67ba9f833a82baf531b36e5ae0c3
Malika-Auma-O <mamajedeia@gmail.com>
2023-11-09 01:22:38 +0200

    changed 404 image

--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -1,16 +1,16 @@
 import Image from 'react-bootstrap/Image';
 import Button from 'react-bootstrap/Button';
-import fofo from "../images/blog404.avif"
+import fofo from "../images/nots.png"
 // import Footer from './Footer';
 
 function NotFound() {
   return (
     <div className="position-relative">
       <Image src={fofo} fluid />
-      <div className="position-absolute top-0 start-0 w-100 h-100 d-flex justify-content-center align-items-center text-warning ">
+      <div className="position-absolute top-0 start-0 w-100 h-100 d-flex justify-content-center align-items-center text-danger ">
         <div>
-        <br></br><br></br>
-        <h1 className='display-1 customBoldText'>Page not found</h1>
+        {/* <br></br><br></br> */}
+        {/* <h1 className='display-1 customBoldText'>Page not found</h1> */}
         
         <Button  variant="primary">Go Home</Button>
         </div>
